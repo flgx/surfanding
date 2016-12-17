b1a8db50a9c675fd2d75d97298aaf601ece19bd2
Fran <fraan.mp@gmail.com>
2016-12-17 12:48:23 -0300

    Navbar created

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="row">
+  <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+	components:{
+		appHeader: Header
+	}
+}
+</script>
+
+<style>
+</style>
